7261c290317accceb192630f166783d062898b5f
Irfan Shah <mail.urfi@gmail.com>
2012-11-30 05:30:19 -0200

    get rid of the comment

--- src/vmod_geoip.c
+++ src/vmod_geoip.c
@@ -9,26 +9,9 @@
 #include "include/vct.h"
 #include "vcc_if.h"
 
-//  Are there any code elements available which I myself can
-//  assign in my own application of ISO 3166-1?  Yes. There are
-//  series of codes which the ISO 3166/MA will never use in the
-//  updating process of // ISO 3166-1 and which are freely
-//  available for users.
-//
-//  To quote from ISO 3166-1:2006, clause 8.1.3 User-assigned code
-//  elements:
-//  ,----
-//  | If users need code elements to represent country names not
-//  | included in this part of ISO 3166, the series of letters
-//  | AA, QM to QZ, XA to XZ, and ZZ, and the series AAA to AAZ,
-//  | QMA to QZZ, XAA to XZZ, and ZZA to ZZZ respectively and the
-//  | series of numbers 900 to 999 are available. These users
-//  | should inform the ISO 3166/MA of such use.
-//  `----
-
 static const char *unknown= "Unknown";
 
-char *trimwhitespace(char *str)
+char* trimwhitespace(char *str)
 {
   char *end;
   while(isspace(*str)) str++;
